0249cc81b107777ddd41df9b9868bccfc2a6fc23
farizz <fairizwalker@gmail.com>
2025-05-08 00:16:39 +0700

    update repositories

--- todoapp/lib/data/models/task.dart
+++ todoapp/lib/data/models/task.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
-import 'package:todoapp/utils/task_category.dart';
+import 'package:todoapp/utils/utils.dart';
 
 class Task extends Equatable {
   final int? id;
@@ -10,15 +9,15 @@ class Task extends Equatable {
   final String date;
   final TaskCategories category;
   final bool isComplated;
-
-  const Task(
-      {this.id,
-      required this.title,
-      required this.note,
-      required this.time,
-      required this.date,
-      required this.category,
-      required this.isComplated});
+  const Task({
+    this.id,
+    required this.title,
+    required this.note,
+    required this.time,
+    required this.date,
+    required this.category,
+    required this.isComplated,
+  });
 
   @override
   List<Object> get props {
@@ -30,5 +29,29 @@ class Task extends Equatable {
       date,
       isComplated,
     ];
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      TaskKeys.id: id,
+      TaskKeys.title: title,
+      TaskKeys.note: note,
+      TaskKeys.time: time,
+      TaskKeys.date: date,
+      TaskKeys.category: category.name,
+      TaskKeys.isComplated: isComplated,
+    };
+  }
+
+  factory Task.fromJson(Map<String, dynamic> map) {
+    return Task(
+      id: map[TaskKeys.id],
+      title: map[TaskKeys.title],
+      note: map[TaskKeys.note],
+      time: map[TaskKeys.time],
+      date: map[TaskKeys.date],
+      category: TaskCategories.stringToCategory(map[TaskKeys.category]),
+      isComplated: map[TaskKeys.isComplated],
+    );
   }
 }
